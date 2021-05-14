36f645424eeecd21f4146273cc086db2842b2424
HarshaPrajapati <harsha_1605@yahoo.com>
2021-05-14 12:32:06 +0530

    changing counter to reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,31 @@
-import React, { useState }  from "react";
+import React, { useReducer }  from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'change_increase':
+      return { ...state, counter: state.counter + action.payload };
+    case 'change_decrease':
+      return { ...state, counter: state.counter + action.payload };
+    default:
+      return state;
+  }
+}
 
 const CounterScreen = ({ navigation }) => {
   //props have navigate fn ..so we pass the string to it which matches the routes
- const [counter, setCounter] = useState(0);
+//  const [counter, setCounter] = useState(0);
+const [state, dispatchReducer] = useReducer(reducer, {counter: 0});
+
+const { counter} = state;
 
   return (
     <View>
     <Button title="Increase" onPress={()=> {
-        setCounter(counter + 1)
+        dispatchReducer({type:'change_increase', payload: 1})
     }}/>
     <Button title="Decrease" onPress={()=> {
-        setCounter(counter - 1)
+        dispatchReducer({type:'change_decrease', payload: -1})
     }}/>
       <Text style={styles.texta}>Current Count: {counter}</Text>
     </View>
